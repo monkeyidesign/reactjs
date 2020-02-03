3cf2196c0f341eecfd8a683c654f09601898d89c
Samreaksa <rsam@adalte.com>
2020-02-03 14:55:36 +0700

    Create reacjs in wordpress template

--- public/index.php
+++ public/index.php
@@ -1,37 +1,26 @@
 <?php
-  $_reactSrc = realpath(__DIR__) . "/react-src";
-  $_publicPath = $_reactSrc . "/public";
-  $_postInstallerPath = $_publicPath . "/post_installer.php";
-
-  if (file_exists($_postInstallerPath)) {
-    // read the package.json file
-    $packagePath = $_reactSrc . "/package.json";
-    $json = json_decode(file_get_contents($packagePath), true);
-
-    // set the "homepage" correctly in the JSON
-    // this ensures all WebPack produced links will work
-    $templatePath = get_template_directory_uri();
-    $templatePath = parse_url($templatePath, PHP_URL_PATH);
-    $json["homepage"] = $templatePath;
-    // set browserLaunchTo to the site's root
-    $json["browserLaunchTo"] = site_url();
-
-    // rewrite the package.json file with the new value for "homepage"
-    $fp = fopen($packagePath, "w");
-    fwrite($fp, json_encode($json, JSON_PRETTY_PRINT));
-    fclose($fp);
-
-    // delete the current index.php page (which is this page you are currently reading)
-    unlink($_publicPath . "/index.php");
-    // rename the "post_installer.php" to make it the new index.php file
-    rename($_postInstallerPath, $_publicPath . "/index.php");
-  }
+  $TEMPLATE_PATH = parse_url(get_template_directory_uri(), PHP_URL_PATH);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+    <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <meta
+      name="description"
+      content="Web site created using create-react-wptheme"
+    />
+    <link rel="apple-touch-icon" href="<?php echo $TEMPLATE_PATH; ?>/logo192.png" />
 
-Now, back in your command prompt, rerun the "start" script again...
-
-<script>
-  setTimeout(function() {
-    window.location.replace(window.location);
-  }, 3000);
-</script>
+    <link rel="manifest" href="<?php echo $TEMPLATE_PATH; ?>/manifest.json" />
+    <title>React WordPress Theme</title>
+</head>
+    <body>
+    <noscript>
+        You need to enable JavaScript to run this app.
+    </noscript>
+    <div id="root"></div>
+    </body>
+</html>
--- src/App.js
+++ src/App.js
@@ -2,25 +2,45 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>react-src/src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            data: []
+        };
+    }
 
+    componentDidMount() {
+       return  fetch("http://localhost/wp-react/wp-json/wp/v2/posts/")
+            .then(res => res.json())
+            .then((result) => {
+                this.setState({
+                    isLoaded: true,
+                    data: result
+                })
+            })
+           .catch((error) => {
+                this.setState({
+                    isLoaded: true,
+                    error
+                });
+           })
+    }
+
+    render() {
+        return (
+            <div className="App">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">
+                    {this.state.data.map((item, i)=>(<p key={i}>{item.title.rendered}</p>) )}
+                </h1>
+              </header>
+            </div>
+        );
+
+    }
+}
 export default App;
